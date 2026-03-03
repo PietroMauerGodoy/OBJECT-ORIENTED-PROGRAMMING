3e7c9b2664ada70eff7c0f629d3a872a6e6f2008
PietroMauerGodoy <godoypietro03@gmail.com>
2026-03-03 20:57:21 -0300

    metodos add

--- POO-AULA2/PrimeiroProjetoCaneta/src/br/com/ProjetoCaneta/model/Caneta.java
+++ POO-AULA2/PrimeiroProjetoCaneta/src/br/com/ProjetoCaneta/model/Caneta.java
@@ -1,5 +1,47 @@
 package br.com.ProjetoCaneta.model;
 
-public class Caneta {
+public class Caneta{
+
+    public String marca;
+    public String cor;
+    public double valor;
+    public String material;
+    public boolean estaTampada;
+    public float quantidadeTinta = 100;
+
+
+
+    public Caneta(String marca, String cor, Double valor, String material) {
+        this.marca = marca ;
+        this.cor = cor;
+        this.valor  = valor;
+        this.material = material;
+    }
+
+    public String estaTampada() {
+        if (this.estaTampada == true) return "tampada";
+        else return "destampada";
+
+    }
+
+    public void destamparCaneta() {
+
+        if (this.estaTampada == false ) {
+            System.out.println("Sua caneta já esta destampada");
+            return;
+        }
+       this.estaTampada = false;
+
+    }
+    public void escrever(String escrever) {
+
+        if (this.estaTampada == false
+                && this.quantidadeTinta >= 10 ) {
+            System.out.println("Voce escreveu: " + escrever);
+
+        }
+    }
+
+
 
 }
--- POO-AULA2/PrimeiroProjetoCaneta/src/br/com/ProjetoCanete/main/Sistema.java
+++ POO-AULA2/PrimeiroProjetoCaneta/src/br/com/ProjetoCanete/main/Sistema.java
@@ -1,10 +1,24 @@
 package br.com.ProjetoCanete.main;
 
+import br.com.ProjetoCaneta.model.Caneta;
+
 public class Sistema {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			Caneta caneta1 = new Caneta("Bic" , "Azul" , 3.0, "plástico");
+			Caneta caneta2 = new Caneta("CIS", "Preto", 5.0, "acrilico");
 
+
+			System.out.println("A caneta é da cor : " + caneta1.cor );
+			System.out.println("A caneta é desse : " + caneta1.marca );
+			System.out.println("A caneta foi esse valor:" + caneta1.valor);
+			System.out.println("A caneta é desse material" + caneta1.material);
+
+
+		System.out.println("Sua caneta esta " + caneta1.estaTampada());
+
+		}
 	}
 
-}
+
+
