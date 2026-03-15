79e365bfa91dbb0823c7b058272a92628d934849
PietroMauerGodoy <godoypietro03@gmail.com>
2026-03-15 14:32:56 -0300

    feat: atualiza README e corrige classe Caneta

--- POO-AULA2/PrimeiroProjetoCaneta/src/br/com/ProjetoCaneta/model/Caneta.java
+++ POO-AULA2/PrimeiroProjetoCaneta/src/br/com/ProjetoCaneta/model/Caneta.java
@@ -7,65 +7,59 @@ public class Caneta {
     private double valor;
     private String material;
     private boolean estaTampada;
-    public float quantidadeTinta = 100;
+    private float quantidadeTinta = 100;
 
-
-    public Caneta(String marca, String cor, Double valor, String material) {
-        this.marca = marca;
-        this.cor = cor;
+    public Caneta(String marca, String cor, double valor, String material) {
+        this.setMarca(marca);
+        this.setCor(cor);
         this.setValor(valor);
         this.material = material;
+        this.estaTampada = true;
     }
 
     public String getEstaTampada() {
-        if (this.estaTampada == true) return "tampada";
-        else return "destampada";
-
+        if (this.estaTampada) {
+            return "tampada";
+        } else {
+            return "destampada";
+        }
     }
+
     public void setEstaTampada(boolean estaTampada){
-            this.estaTampada = estaTampada;
+        this.estaTampada = estaTampada;
     }
-
-
 
     public void destamparCaneta() {
-
-        if (this.estaTampada == false) {
-            System.out.println("Sua caneta já esta destampada");
+        if (!this.estaTampada) {
+            System.out.println("Sua caneta já está destampada");
             return;
         }
         this.estaTampada = false;
-
+        System.out.println("Caneta destampada.");
     }
 
     public void escrever(String texto) {
-
         if (this.estaTampada) {
             System.out.println("Não é possível escrever. A caneta está tampada.");
             return;
         }
-
         if (this.quantidadeTinta < 10) {
             System.out.println("Tinta insuficiente.");
             return;
         }
-
         System.out.println("Você escreveu: " + texto);
-        this.quantidadeTinta -= 10; // altera o estado
+        this.quantidadeTinta -= 10;
     }
 
     public void setValor(double valor) {
-
-       if (this.valor > 0){
-           this.valor = valor;
-       } else {
-           System.out.println("Valor inválido");
-       }
-
+        if (valor > 0){
+            this.valor = valor;
+        } else {
+            System.out.println("Valor inválido");
+        }
     }
 
     public double getValor(){
-
         return this.valor;
     }
 
@@ -73,29 +67,31 @@ public class Caneta {
         return this.marca;
     }
 
-
-    public void setMarca (String marca){
-        if (marca != "") {
+    public void setMarca(String marca){
+        if (marca != null && !marca.isEmpty()) {
             this.marca = marca;
-        }else {
-        System.out.println("Marca inválida!");
-        }
-    }
-    public String getCor(){
-        return this.cor;
-    }
-
-
-    public void setCor (String cor){
-        if (cor != "") {
-            this.cor = marca;
-        }else {
+        } else {
             System.out.println("Marca inválida!");
         }
     }
 
+    public String getCor(){
+        return this.cor;
+    }
 
+    public void setCor(String cor){
+        if (cor != null && !cor.isEmpty()) {
+            this.cor = cor;
+        } else {
+            System.out.println("Cor inválida!");
+        }
+    }
 
+    public float getQuantidadeTinta() {
+        return quantidadeTinta;
+    }
 
-
+    public String getMaterial() {
+        return this.material;
+    }
 }
--- POO-AULA2/PrimeiroProjetoCaneta/src/br/com/ProjetoCanete/main/Sistema.java
+++ POO-AULA2/PrimeiroProjetoCaneta/src/br/com/ProjetoCanete/main/Sistema.java
@@ -1,25 +1,28 @@
-package br.com.ProjetoCanete.main;
+
 
 import br.com.ProjetoCaneta.model.Caneta;
 
-public class Sistema {
 
-	public static void main(String[] args) {
-			Caneta caneta1 = new Caneta("Bic" , "Azul" , 3.0, "plástico");
-			Caneta caneta2 = new Caneta("CIS", "Preto", 5.0, "acrilico");
+public static void main(String[] args) {
 
+	Caneta caneta1 = new Caneta("Bic", "Azul", 3.0, "plástico");
+	Caneta caneta2 = new Caneta("CIS", "Preto", 5.0, "acrílico");
 
-			System.out.println("A caneta é da cor : " + caneta1.getCor() );
-			System.out.println("A caneta é desse : " + caneta1.getCor() );
-			System.out.println("A caneta foi esse valor :" + caneta1.getCor());
-			System.out.println("A caneta é desse material : " + caneta1.getCor());
+	System.out.println("A marca da caneta é: " + caneta1.getMarca());
+	System.out.println("A cor da caneta é: " + caneta1.getCor());
+	System.out.println("O valor da caneta foi: R$ " + caneta1.getValor());
+	System.out.println("O material da caneta é: " + caneta1.getMaterial());
 
-
-			System.out.println("Sua caneta esta " + caneta1.getEstaTampada());
-			System.out.println("Agora vc pode escrever, sua caneta está com " + caneta1.quantidadeTinta + "% de tinta!");
-
-		}
-	}
+	System.out.println("Sua caneta está " + caneta1.getEstaTampada());
 
 
+	caneta1.escrever("Olá mundo!");
 
+
+	caneta1.destamparCaneta();
+	System.out.println("Sua caneta está " + caneta1.getEstaTampada());
+
+
+	caneta1.escrever("Olá mundo!");
+	System.out.println("Tinta restante: " + caneta1.getQuantidadeTinta() + "%");
+}
